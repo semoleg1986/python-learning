3ebd6507fa20340a6ec518b822ae16b510a79e86
semoleg1986 <semoleg1986@gmail.com>
2025-09-30 12:40:56 +0400

    feat: add docstring(Sphinx style) (Tue, Sep 30, 2025 12:40:56 PM)

--- week1/day1_numbers.py
+++ week1/day1_numbers.py
@@ -5,14 +5,40 @@ Day 1 — Numbers practice
 
 
 def is_even(n: int) -> bool:
+    """
+    Проверяет, является ли число чётным.
+
+    :param n: Целое число.
+    :type n: int
+    :return: True, если число чётное, иначе False.
+    :rtype: bool
+    """
     return n % 2 == 0
 
 
 def digit_sum(s: int) -> int:
+    """
+    Вычисляет сумму цифр целого числа.
+
+    :param s: Целое число
+    :type s: int
+    :return: Сумма цифр числа.
+    :rtype: int
+    """
     return sum(int(i) for i in str(s))
 
 
 def factorial(n: int) -> int:
+    """
+    Вычисляет факториал числа n (n!).
+
+    Реализовано через цикл.
+
+    :param n: Целое неотрицательное число.
+    :type n: int
+    :return: Факториал числа n.
+    :rtype: int
+    """
     s = 1
     for i in range(1, n + 1):
         s *= i
@@ -20,6 +46,16 @@ def factorial(n: int) -> int:
 
 
 def factorial2(n: int) -> int:
+    """
+    Вычисляет факториал числа n (n!).
+
+    Реализовано через рекурсивный вызов функции.
+
+    :param n: Целое неотрицательное число.
+    :type n: int
+    :return: Факториал числа n.
+    :rtype: int
+    """
     if n == 0:
         return 1
     return n * factorial2(n - 1)
--- week1/day2_strings.py
+++ week1/day2_strings.py
@@ -6,15 +6,41 @@ import re
 
 
 def reverse_string(s: str) -> str:
+    """
+    Возвращает строку в обратном порядке.
+
+    :param s: Исходная строка.
+    :type s: str
+    :return: Строка, где символы идут в обратном порядке.
+    :rtype: str
+    """
     return "".join(reversed(list(s)))
 
 
 def is_palindrome(s: str) -> bool:
+    """
+    Проверяет, является ли строка палиндромом.
+
+    Игнорирует регистр символов и неалфавитные символы.
+
+    :param s: Исходная строка.
+    :type s: str
+    :return: True, если строка является палиндромом, иначе False.
+    :rtype: bool
+    """
     cleaned = re.sub(r"[^\w]", "", s).lower()
     return cleaned == cleaned[::-1]
 
 
 def count_vowels(s: str) -> int:
+    """
+    Считает количество гласных букв в строке.
+
+    :param s: Исходная строка.
+    :type s: str
+    :return: Количество гласных в строке.
+    :rtype: int
+    """
     vowels = ["a", "e", "i", "o", "u"]
     count = 0
     for i in s.lower():
