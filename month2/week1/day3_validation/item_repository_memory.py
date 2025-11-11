d81e310c1d728e9551c2c73f4e17b18124fde16c
semoleg1986 <semoleg1986@gmail.com>
2025-11-11 13:27:32 +0400

    feat: impl. items storage(ItemRepository)(Tue, Nov 11, 2025 01:27:32 PM)

---
+++ month2/week1/day3_validation/item_repository_memory.py
@@ -0,0 +1,26 @@
+from typing import Dict, List, Optional
+from uuid import UUID
+
+from models import ItemModel
+
+items = Dict[UUID, ItemModel]
+
+
+class ItemRepositoryMemory:
+    """Хранилище для Item"""
+
+    def __init__(self):
+        self._storage: Dict[UUID, ItemModel] = {}
+
+    def save(self, item: ItemModel) -> ItemModel:
+        self._storage[item.id] = item
+        return item
+
+    def get(self, item_id: UUID) -> Optional[ItemModel]:
+        return self._storage.get(item_id)
+
+    def list_all(self) -> List[ItemModel]:
+        return list(self._storage.values())
+
+    def delete(self, item_id: UUID) -> bool:
+        return self._storage.pop(item_id, None) is not None
