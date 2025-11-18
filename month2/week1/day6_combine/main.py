f06c9ef2f7d902337d4f6efe7138e7d7be0564c3
semoleg1986 <semoleg1986@gmail.com>
2025-11-18 17:51:30 +0400

    feat(day6): add main and info route (Tue, Nov 18, 2025 05:51:30 PM)

---
+++ month2/week1/day6_combine/main.py
@@ -0,0 +1,33 @@
+from fastapi import Depends, FastAPI
+
+from month2.week1.day6_combine.infrastructure.config.settings import Settings, settings
+
+tags_metadata = [
+    {"name": "info", "description": "Эндпоинты для информации о приложении"},
+    {
+        "name": "items",
+        "description": "Эндпоинты для работы с товарами.",
+    },
+]
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.VERSION,
+    description=settings.DESCRIPTION,
+    openapi_tags=tags_metadata,
+)
+
+
+@app.get(
+    "/info",
+    tags=["info"],
+    summary="Информация о приложении",
+    description="Возвращает имя приложения и его версию.",
+    response_model=dict,
+    responses={200: {"description": "Информация успешно получена"}},
+)
+def info(cfg: Settings = Depends(lambda: settings)):
+    """
+    Эндпоинт возвращает базовую информацию о приложении.
+    """
+    return {"app_name": cfg.APP_NAME, "version": cfg.VERSION}
